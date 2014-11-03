c2820747368268030477dca81a9a00fb366b9b15
irwinna <nathan.irwin@vuw.ac.nz>
2014-11-03 16:28:26 +1300

    Rage test

--- app.js
+++ app.js
@@ -1,78 +1,85 @@
-'use strict';
+// 'use strict';
 
-/*
- * Express Dependencies
- */
-var express = require('express');
-var app = express();
-var port = 3000;
+// /*
+//  * Express Dependencies
+//  */
+// var express = require('express');
+// var app = express();
+// var port = 3000;
 
-/*
- * Use Handlebars for templating
- */
-var exphbs = require('express-handlebars');
-var hbs;
+// /*
+//  * Use Handlebars for templating
+//  */
+// var exphbs = require('express-handlebars');
+// var hbs;
 
-// For gzip compression
-app.use(express.compress());
+// // For gzip compression
+// app.use(express.compress());
 
-/*
- * Config for Production and Development
- */
-if (process.env.NODE_ENV === 'production') {
-    // Set the default layout and locate layouts and partials
-    app.engine('handlebars', exphbs({
-        defaultLayout: 'main',
-        layoutsDir: 'dist/views/layouts/',
-        partialsDir: 'dist/views/partials/'
-    }));
+// /*
+//  * Config for Production and Development
+//  */
+// if (process.env.NODE_ENV === 'production') {
+//     // Set the default layout and locate layouts and partials
+//     app.engine('handlebars', exphbs({
+//         defaultLayout: 'main',
+//         layoutsDir: 'dist/views/layouts/',
+//         partialsDir: 'dist/views/partials/'
+//     }));
 
-    // Locate the views
-    app.set('views', __dirname + '/dist/views');
+//     // Locate the views
+//     app.set('views', __dirname + '/dist/views');
     
-    // Locate the assets
-    app.use(express.static(__dirname + '/dist/assets/'));
+//     // Locate the assets
+//     app.use(express.static(__dirname + '/dist/assets/'));
 
-} else {
-    app.engine('handlebars', exphbs({
-        // Default Layout and locate layouts and partials
-        defaultLayout: 'main',
-        layoutsDir: 'views/layouts/',
-        partialsDir: [
-            'views/partials/',
-            'views/partials/_inc',
-            'views/partials/atoms',
-            'views/partials/_2_mocules',
-            'views/partials/_3_organisms'
-        ]
-    }));
+// } else {
+//     app.engine('handlebars', exphbs({
+//         // Default Layout and locate layouts and partials
+//         defaultLayout: 'main',
+//         layoutsDir: 'views/layouts/',
+//         partialsDir: [
+//             'views/partials/',
+//             'views/partials/_inc',
+//             'views/partials/atoms',
+//             'views/partials/_2_mocules',
+//             'views/partials/_3_organisms'
+//         ]
+//     }));
 
-    // Locate the views
-    app.set('views', __dirname + '/views');
+//     // Locate the views
+//     app.set('views', __dirname + '/views');
     
-    // Locate the assets
-    app.use('/assets', express.static(__dirname + '/assets'));
-}
+//     // Locate the assets
+//     app.use('/assets', express.static(__dirname + '/assets'));
+// }
 
-// Set Handlebars
-app.set('view engine', 'handlebars');
+// // Set Handlebars
+// app.set('view engine', 'handlebars');
 
 
 
-/*
- * Routes
- */
-// Index Page
-app.get('/', function (request, response, next) {
-    response.render('index');
-    console.log('bootie');
+// /*
+//  * Routes
+//  */
+// // Index Page
+// app.get('/', function (request, response, next) {
+//     response.render('index');
+//     console.log('bootie');
   
-});
+// });
 
 
-/*
- * Start it up
- */
-app.listen(process.env.PORT || port);
-console.log('Express started on port ' + port);
+// /*
+//  * Start it up
+//  */
+// app.listen(process.env.PORT || port);
+// console.log('Express started on port ' + port);
 // console.log(process.env.NODE_ENV);
+
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
