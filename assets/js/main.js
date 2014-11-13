1280813c1c08d4494f6e95f28bfe861d17b36902
irwinna <nathan.irwin@vuw.ac.nz>
2014-11-13 15:22:05 +1300

    Adding more to elements page

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+
+
+
+$(function() {
+	$('#page').on('click', '.code_hider', function(e) {
+
+		$this = $(this);
+		
+		var $arrow = $this.find('.code_toggle');
+		$arrow.toggleClass('open');
+		
+		var $pre = $this.nextAll('pre');
+		$pre.toggleClass('hido');
+
+	});
+});
